7161652afbda8e06249fbbd0bed5cf9e4406d62c
IvanDirigible <sickeruncible@gmail.com>
2023-12-18 19:51:34 -0500

    Added API connectivity and utilities.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,57 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { FoodTruck, User } = require('../model');
 
-// More later
+router.get('/', async (req, res) => {
+    try {
+        const foodtruckData = await FoodTruck.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ["name"],
+                },
+            ],
+        });
+
+        const foodtrucks = foodtruckData.map((foodtruck) => foodtruck.get({ plain: true }));
+
+        res.render("homepage", {
+            foodtrucks,
+
+        });
+    } catch (err) {
+        console.log("Error Here");
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+
+router.get('/foodtruck/:id', async (req, res) => {
+    try {
+        const foodtruckData = await FoodTruck.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ["name"],
+                },
+            ],
+        });
+
+        const foodtruck = foodtruckData.get({ plain: true });
+
+        res.render('foodtruck', {
+            ...foodtruck,
+
+        });
+    } catch (err) {
+        console.log("Error Over Here");
+        res.status(500).json(err);
+    }
+});
+
+//middleware goes here
+
+router.get('login', (req, res) => {
+    res.render('login');
+});
 
 module.exports = router;
